e366fd6f8175e9431354abf6ec7e4f31264c4151
Bradley73 <bradstan@live.co.uk>
2026-01-03 15:59:45 +0000

    added yaml for champion state table

--- projects/raid/transform/models/gold/account/account_champion_state.sql
+++ projects/raid/transform/models/gold/account/account_champion_state.sql
@@ -1,45 +1,55 @@
 {{ config(
-    materialize = 'view'
+    materialized = 'view',
     alias = 'gold_account_champion_state'
 ) }}
 
 WITH base AS (
-    SELECT DISTINCT
-        account_name,
-        champion_id
+    SELECT account_name, champion_id,
+        SUM(is_current) AS total_owned,
+        SUM(is_deleted) AS total_deleted,
+        MAX(empower_level) AS max_empower_level,
+        MAX(blessing_id) AS max_blessing_level,
+        MAX(rank) AS max_rank
     FROM {{ ref("champindex_scd2") }}
+    GROUP BY account_name, champion_id
 ),
 
-agg AS (
+events_obtained AS (
+    SELECT account_name, champion_id,
+        COUNT(*) AS total_obtained
+    FROM {{ ref('champindex_events_obtained') }}
+    GROUP BY account_name, champion_id
+),
+
+events_upgraded AS (
+    SELECT account_name, champion_id,
+        MAX(event_ts) AS last_upgraded_ts
+    FROM {{ ref('champindex_events_upgraded') }}
+    GROUP BY account_name, champion_id
+),
+
+final AS (
     SELECT
         b.*,
-        MAX(u.event_ts) AS last_upgraded_ts,
-        MAX(scd.is_current) AS is_owned,
-        SUM(scd.is_deleted) AS total_deleted,
-        SUM(scd.is_current) AS duplicate_count,
-        COUNT(o.*) AS total_seen,
-        MAX(scd.empower_level) AS empower_level,
-        MAX(scd.blessing_id) AS blessing_level,
-        MAX(scd.rank) AS highest_rank,
+        o.total_obtained,
+        u.last_upgraded_ts,
         cfs.first_seen_ts,
         cl.champion_name,
         cl.rarity,
         cl.affinity,
         cl.faction
-    FROM base
-    INNER JOIN {{ ref("champion_lookup") }} cl
-    INNER JOIN {{ ref('champindex_events_upgraded') }} u
+    FROM base b
+    LEFT JOIN events_obtained o
+        ON b.account_name = o.account_name
+           AND b.champion_id = o.champion_id
+    LEFT JOIN events_upgraded u
         ON b.account_name = u.account_name
            AND b.champion_id = u.champion_id
-    INNER JOIN {{ ref('champindex_events_obtained') }} o
-        ON b.account_name = u.account_name
-           AND b.champion_id = u.champion_id
-    INNER JOIN {{ ref("champion_first_seen") }} cfs
-        ON b.account_name = c.account_name
-           AND b.champion_id = c.champion_id
-    INNER JOIN {{ ref("champindex_scd2") }} scd
-        ON b.account_name = scd.account_name
-           AND b.champion_id = scd.champion_id
+    JOIN {{ ref("champion_first_seen") }} cfs
+        ON b.account_name = cfs.account_name
+           AND b.champion_id = cfs.champion_id
+    JOIN {{ ref("champion_lookup") }} cl
+        ON b.champion_id = cl.champion_id
 )
 
 SELECT
@@ -48,14 +58,13 @@ SELECT
     rarity,
     affinity,
     faction,
-    is_owned,
     first_seen_ts,
     last_upgraded_ts,
-    total_seen,
+    total_owned,
     total_deleted,
-    duplicate_count,
-    highest_rank,
-    empower_level,
-    blessing_level
-FROM agg
+    total_obtained,
+    max_rank,
+    max_empower_level,
+    max_blessing_level
+FROM final
 ;
