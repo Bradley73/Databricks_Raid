56e4fce022f89a0acbc0332111cd2b23d1cc279f
Bradley73 <bradstan@live.co.uk>
2026-01-04 19:50:40 +0000

    fixed parse errors and gold refs

--- projects/raid/transform/models/gold/account/account_champion_state.sql
+++ projects/raid/transform/models/gold/account/account_champion_state.sql
@@ -4,28 +4,28 @@
 ) }}
 
 WITH base AS (
-    SELECT account_name, champion_id,
+    SELECT account_name, champion_key,
         SUM(CASE WHEN is_current THEN 1 ELSE 0 END) AS total_owned,
         SUM(CASE WHEN is_deleted THEN 1 ELSE 0 END) AS total_deleted,
         MAX(empower_level) AS max_empower_level,
         MAX(blessing_id) AS max_blessing_level,
         MAX(rank) AS max_rank
     FROM {{ ref("champindex_scd2") }}
-    GROUP BY account_name, champion_id
+    GROUP BY account_name, champion_key
 ),
 
 events_obtained AS (
-    SELECT account_name, champion_id,
+    SELECT account_name, champion_key,
         COUNT(*) AS total_obtained
     FROM {{ ref('champindex_events_obtained') }}
-    GROUP BY account_name, champion_id
+    GROUP BY account_name, champion_key
 ),
 
 events_upgraded AS (
-    SELECT account_name, champion_id,
+    SELECT account_name, champion_key,
         MAX(event_ts) AS last_upgraded_ts
     FROM {{ ref('champindex_events_upgrade') }}
-    GROUP BY account_name, champion_id
+    GROUP BY account_name, champion_key
 ),
 
 final AS (
@@ -41,20 +41,20 @@ final AS (
     FROM base b
     LEFT JOIN events_obtained o
         ON b.account_name = o.account_name
-           AND b.champion_id = o.champion_id
+           AND b.champion_key = o.champion_key
     LEFT JOIN events_upgraded u
         ON b.account_name = u.account_name
-           AND b.champion_id = u.champion_id
+           AND b.champion_key = u.champion_key
     JOIN {{ ref("champion_first_seen") }} cfs
         ON b.account_name = cfs.account_name
-           AND b.champion_id = cfs.champion_id
+           AND b.champion_key = cfs.champion_key
     JOIN {{ ref("champion_lookup") }} cl
-        ON b.champion_id = cl.champion_id
+        ON b.champion_key = cl.champion_key
 )
 
 SELECT
     account_name,
-    champion_id,
+    champion_key,
     champion_name,
     rarity,
     affinity,
